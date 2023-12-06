430c3f6ae5fbf38862481fe7946778e5b94efe43
Whenny Zenica <151903699+Whanyzencc@users.noreply.github.com>
2023-12-06 21:05:25 +0700

    Add files via upload

---
+++ Jobsheet 1_Whenny Zenica_23343022/js1 Percobaan Codingan 1.cpp
@@ -0,0 +1,12 @@
+// created by Whenny Zenica 233430622
+
+#include <stdio.h> // header yang berisikan library
+
+int main(){ // tempat program dieksekusi
+	
+	// menampilkan output Hello World
+	printf("Hello world");
+	
+	// membalikkan nilai 0
+	return 0;
+}
